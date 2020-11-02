95e15c0c06fbb34aca8ca90c1418b08c7d943596
BORBECH <charlieborbs18@gmail.com>
2020-11-02 19:08:17 +0800

    [FrontEnd][Charlie] fix : Added additional column for Parking Lot Name and Header

--- src/components/MPHeader.js
+++ src/components/MPHeader.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-
 import { Layout, Menu } from 'antd';
 
 import logo from '../images/minuteman-V2.png';
--- src/components/ViewTransactionHistory.js
+++ src/components/ViewTransactionHistory.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import MPHeader from './MPHeader';
 import { Table } from 'antd';
 
 class ViewTransactionHistory extends Component {
@@ -11,13 +12,19 @@ class ViewTransactionHistory extends Component {
                 render: text => <b>{text}</b>,
             },
             {
+                title: 'Parking Lot Name',
+                dataIndex: 'parkingLotName',
+                key: 'text',
+                render: text => <b>{text}</b>,
+            },
+            {
                 title: 'Ticket ID',
                 dataIndex: 'ticketId',
                 key: 'id',
                 render: text => <b>{text}</b>,
             },
             {
-                title: 'Trasanction Date',
+                title: 'Trasaction Date',
                 dataIndex: 'transactDate',
                 key: 'id',
                 render: text => <b>{text}</b>,
@@ -26,72 +33,84 @@ class ViewTransactionHistory extends Component {
         const dataSource = [
             {
                 key: '1',
-                transactId: '1',            
+                parkingLotName: 'A-01',
+                transactId: '1',
                 ticketId: '1',
                 transactDate: '2019-11-02',
             },
             {
                 key: '2',
+                parkingLotName: 'A-02',
                 transactId: '2',
                 ticketId: '2',
                 transactDate: '2019-11-02',
             },
             {
                 key: '3',
+                parkingLotName: 'A-03',
                 transactId: '3',
                 ticketId: '2',
                 transactDate: '2019-11-02',
             },
             {
                 key: '4',
+                parkingLotName: 'A-04',
                 transactId: '4',
                 ticketId: '2',
                 transactDate: '2019-11-02',
             },
             {
                 key: '5',
+                parkingLotName: 'A-05',
                 transactId: '5',
                 ticketId: '2',
                 transactDate: '2019-11-02',
             },
             {
                 key: '6',
+                parkingLotName: 'B-01',
                 transactId: '6',
                 ticketId: '2',
                 transactDate: '2019-11-02',
             },
             {
                 key: '7',
+                parkingLotName: 'B-02',
                 transactId: '7',
                 ticketId: '2',
                 transactDate: '2019-11-02',
             },
             {
                 key: '8',
+                parkingLotName: 'B-03',
                 transactId: '8',
                 ticketId: '2',
                 transactDate: '2019-11-02',
             },
             {
                 key: '9',
+                parkingLotName: 'B-04',
                 transactId: '9',
                 ticketId: '2',
                 transactDate: '2019-11-02',
             },
             {
                 key: '10',
+                parkingLotName: 'B-05',
                 transactId: '10',
                 ticketId: '2',
                 transactDate: '2019-11-02',
             },
             {
                 key: '11',
+                parkingLotName: 'C-01',
                 transactId: '11',
                 ticketId: '2',
                 transactDate: '2019-11-02',
             },
             {
                 key: '12',
+                parkingLotName: 'C-02',
                 transactId: '12',
                 ticketId: '2',
                 transactDate: '2019-11-02',
@@ -99,12 +118,15 @@ class ViewTransactionHistory extends Component {
         ];
 
         return (
-            <div className = "table-div">
-                <Table id="done" 
+            <div className="table-div">
+                <div className="first">
+                    <MPHeader />
+                </div>
+                <Table id="done"
                     columns={columns}
                     dataSource={dataSource}
-                    pagination={{ pageSize: 10 }} 
-                    scroll={{ y: 500 }} />
+                    pagination={{ pageSize: 10 }}
+                    scroll={{ y: 550 }} />
             </div>
         );
     }
