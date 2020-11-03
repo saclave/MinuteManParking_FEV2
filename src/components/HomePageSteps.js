ef31151241af20b399ae54c07febfc5215184269
bacaljo <bacaljo@oocl.com>
2020-11-03 09:33:12 +0800

    [FrontEnd][John-Vea] feature : Make main page steps responsive

--- src/components/HomePageSteps.js
+++ src/components/HomePageSteps.js
@@ -6,11 +6,31 @@ import HomePageContentTitle from './HomePageContentTitle';
 const { Step } = Steps;
 
 class HomePageSteps extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { width: 0 };
+    }
+
+    componentDidMount() {
+        this.updateWindowDimensions();
+        window.addEventListener('resize', this.updateWindowDimensions);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('resize', this.updateWindowDimensions);
+    }
+
+    updateWindowDimensions = () => {
+        this.setState({ width: window.innerWidth });
+    }
+
     render() {
+        const className = this.state.width >= 992 ? 'horizontal' : 'vertical';
+
         return (
             <div className="home-page-content container">
                 <HomePageContentTitle id="how_it_works" text="HOW IT WORKS" />
-                <Steps>
+                <Steps direction={className}>
                     <Step title="Authenticate" description="Login to your account or register." />
                     <Step title="Search" description="Ping your location to find the nearest parking lots around you. You can also search for your desired parking lot using our map." />
                     <Step title="Choose" description="Select one to check for its available slots and reservation rate." />
