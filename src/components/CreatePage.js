6479e2eff67ab0f4536478386499295311610883
bacaljo <bacaljo@oocl.com>
2020-11-04 00:34:52 +0800

    [FrontEnd][Jonel-Vea-Laurence] Feature : Enhance design of registration form

--- src/components/CreatePage.js
+++ src/components/CreatePage.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
-import { Form, Input, Button, DatePicker, Select } from 'antd';
-import { v4 as uuidv4 } from 'uuid';
 import { Redirect } from "react-router-dom";
+
+import Highlighter from './Highlighter';
+
+import {
+    Form, Input, Button, DatePicker,
+    Select, Row, Col, Typography
+} from 'antd';
+
+import { v4 as uuidv4 } from 'uuid';
 
 import { UserOutlined, LockOutlined, MailOutlined } from '@ant-design/icons';
 
 const { Option } = Select;
+const { Title } = Typography;
 
 class CreatePage extends Component {
     constructor(props) {
@@ -33,96 +41,104 @@ class CreatePage extends Component {
         }
 
         return (
-            <div className="headerless-page-content">
-                <Form name="normal_login"
-                    className="login-form"
-                    initialValues={{ remember: true }}
-                    onFinish={this.onFinish}>
+            <Row align="middle" className="headerless-page-content">
+                <Col
+                    xs={{ span: 14, offset: 5 }}
+                    sm={{ span: 12, offset: 6 }}
+                    md={{ span: 10, offset: 7 }}
+                    lg={{ span: 8, offset: 8 }}
+                    xl={{ span: 6, offset: 9 }}>
+                    <Title>Become a MINUTE<Highlighter>MAN</Highlighter></Title>
+                    <Form name="normal_login"
+                        className="login-form"
+                        initialValues={{ remember: true }}
+                        onFinish={this.onFinish}>
 
-                    <Form.Item
-                        name="firstName"
-                        rules={[{ required: true, message: 'Please input your First Name!' }]}
-                    >
-                        <Input placeholder="First Name" />
-                    </Form.Item>
-
-                    <Form.Item
-                        name="lastName"
-                        rules={[{ required: true, message: 'Please input your Last Name!' }]}
-                    >
-                        <Input placeholder="Last Name" />
-                    </Form.Item>
-
-                    <Form.Item
-                        name="username"
-                        rules={[{ required: true, message: 'Please input your Username!' }]}
-                    >
-                        <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
-                    </Form.Item>
-
-                    <Form.Item
-                        name="password"
-                        rules={[
-                            {
-                                required: true,
-                                message: 'Please input your Password!',
-                            },
-                        ]}
-                    >
-                        <Input
-                            prefix={<LockOutlined className="site-form-item-icon" />}
-                            type="password"
-                            placeholder="Password"
-                        />
-                    </Form.Item>
-
-                    <Form.Item
-                        name="email"
-                        rules={[
-                            {
-                                required: true,
-                                message: 'Please input your Email!',
-                            },
-                        ]}
-                    >
-                        <Input
-                            prefix={<MailOutlined />}
-                            type="email"
-                            placeholder="Email"
-                        />
-                    </Form.Item>
-
-                    <Form.Item
-                        name="gender"
-                        rules={[{ required: true, message: 'Please select your Gender!' }]}>
-                        <Select
-                            allowClear
-                            placeholder="Gender"
-                            style={{ textAlign: 'left' }}
-                            required
+                        <Form.Item
+                            name="firstName"
+                            rules={[{ required: true, message: 'Please input your First Name!' }]}
                         >
-                            <Option value="Male">Male</Option>
-                            <Option value="Female">Female</Option>
-                            <Option value="Other">Other</Option>
-                        </Select>
-                    </Form.Item>
+                            <Input placeholder="First Name" />
+                        </Form.Item>
 
-                    <Form.Item name="birthDate"
-                        rules={[{ required: true, message: 'Please select your Birth Date!' }]}>
-                        <DatePicker
-                            style={{ width: '100%' }}
-                            placeholder="Birth Date"
-                            label="Birthdate"
-                        />
-                    </Form.Item>
+                        <Form.Item
+                            name="lastName"
+                            rules={[{ required: true, message: 'Please input your Last Name!' }]}
+                        >
+                            <Input placeholder="Last Name" />
+                        </Form.Item>
 
-                    <Form.Item>
-                        <Button type="primary" htmlType="submit" block>
-                            Register
+                        <Form.Item
+                            name="username"
+                            rules={[{ required: true, message: 'Please input your Username!' }]}
+                        >
+                            <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
+                        </Form.Item>
+
+                        <Form.Item
+                            name="password"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: 'Please input your Password!',
+                                },
+                            ]}
+                        >
+                            <Input
+                                prefix={<LockOutlined className="site-form-item-icon" />}
+                                type="password"
+                                placeholder="Password"
+                            />
+                        </Form.Item>
+
+                        <Form.Item
+                            name="email"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: 'Please input your Email!',
+                                },
+                            ]}
+                        >
+                            <Input
+                                prefix={<MailOutlined />}
+                                type="email"
+                                placeholder="Email"
+                            />
+                        </Form.Item>
+
+                        <Form.Item
+                            name="gender"
+                            rules={[{ required: true, message: 'Please select your Gender!' }]}>
+                            <Select
+                                allowClear
+                                placeholder="Gender"
+                                style={{ textAlign: 'left' }}
+                                required
+                            >
+                                <Option value="Male">Male</Option>
+                                <Option value="Female">Female</Option>
+                                <Option value="Other">Other</Option>
+                            </Select>
+                        </Form.Item>
+
+                        <Form.Item name="birthDate"
+                            rules={[{ required: true, message: 'Please select your Birth Date!' }]}>
+                            <DatePicker
+                                style={{ width: '100%' }}
+                                placeholder="Birth Date"
+                                label="Birthdate"
+                            />
+                        </Form.Item>
+
+                        <Form.Item>
+                            <Button type="primary" htmlType="submit" block>
+                                Register
                     </Button>
-                    </Form.Item>
-                </Form>
-            </div >
+                        </Form.Item>
+                    </Form>
+                </Col>
+            </Row>
         );
     }
 }
