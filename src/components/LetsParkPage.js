d82af09cac687a1b5ebfe9d02b738186d1751f9b
bacaljo <bacaljo@oocl.com>
2020-11-04 18:41:02 +0800

    [FrontEnd][John] Feature : Add dummy map in map section

--- src/components/LetsParkPage.js
+++ src/components/LetsParkPage.js
@@ -1,29 +1,33 @@
 import React, { Component } from 'react';
 
+import HomePageFooter from './HomePageFooter';
+
 import MPHeaderContainer from '../containers/MPHeaderContainer';
 
 import { Link } from 'react-router-dom';
-import { Layout, Input } from 'antd';
+import { Layout, Input, Image, Row, Col } from 'antd';
 
-const { Content, Sider } = Layout;
+import sampleMap from '../images/sample-map.png';
+
+const { Sider, Content } = Layout;
 const { Search } = Input;
 
 class LetsParkPage extends Component {
     render() {
         return (
-            <Layout>
-                <MPHeaderContainer />
-                <Content>
-                    <Sider theme="light" width={300}
-                        style={{ marginTop: 65, padding: 20 }}>
-                        <Search placeholder="Search for parking lots here" />
+            <>
+                <Layout>
+                    <MPHeaderContainer />
+                    <Sider theme="light" width={300} style={{ marginTop: 65 }}>
+                        <Search style={{ padding: 20 }} placeholder="Search for parking lots here" />
                     </Sider>
-                    <Content>
-                        <Link smooth to={{ pathname: '/reserve' }}>reserve</Link>
-                        <Link smooth to={{ pathname: '/update' }}>update</Link>
-                    </Content>
-                </Content>
-            </Layout>
+                    <div className="park-page-map-container" style={{ marginTop: 65 }}>
+                        <Image src={sampleMap}></Image>
+                    </div>
+                </Layout>
+                <Link smooth to={{ pathname: '/reserve' }}>reserve</Link>
+                <Link smooth to={{ pathname: '/update' }}>update</Link>
+            </>
         );
     }
 }
